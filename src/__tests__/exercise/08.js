8aa8212d297ebb9d4f8d90167b8aae363cbe545b
Kent C. Dodds <me+github@kentcdodds.com>
2020-04-07 17:04:56 -0600

    finish 08. All done

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -3,17 +3,19 @@
 import React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import UseCounterHookExample from '../../examples/counter-hook'
+import useCounter from '../../components/use-counter'
+
+// 🐨 create a simple function component that uses the useCounter hook
+// and then exposes some UI that our test can interact with to test the
+// capabilities of this hook
+// 💰 here's how ot use the hook:
+// const {count, increment, decrement} = useCounter()
 
 test('exposes the count and increment/decrement functions', () => {
-  render(<UseCounterHookExample />)
-  const increment = screen.getByText(/increment/i)
-  const decrement = screen.getByText(/decrement/i)
-  const message = screen.getByText(/current count/i)
-
-  expect(message).toHaveTextContent('Current count: 0')
-  userEvent.click(increment)
-  expect(message).toHaveTextContent('Current count: 1')
-  userEvent.click(decrement)
-  expect(message).toHaveTextContent('Current count: 0')
+  // 🐨 render the component
+  // 🐨 get the elements you need using screen
+  // 🐨 assert on the initial state of the hook
+  // 🐨 interact with the UI using userEvent and assert on the changes in the UI
 })
+
+/* eslint no-unused-vars:0 */
--- src/__tests__/final/08.js
+++ src/__tests__/final/08.js
@@ -3,7 +3,18 @@
 import React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import UseCounterHookExample from '../../examples/counter-hook'
+import useCounter from '../../components/use-counter'
+
+function UseCounterHookExample() {
+  const {count, increment, decrement} = useCounter()
+  return (
+    <div>
+      <div>Current count: {count}</div>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={increment}>Increment</button>
+    </div>
+  )
+}
 
 test('exposes the count and increment/decrement functions', () => {
   render(<UseCounterHookExample />)
