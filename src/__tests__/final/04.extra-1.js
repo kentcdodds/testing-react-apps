49bcb58e97f878cbc773ce6fe9a888dab635d59c
Gautam Pahuja <32642691+gautam-pahuja@users.noreply.github.com>
2020-04-16 22:42:58 +0200

    added await in final soln and fix type (#6)

--- src/__tests__/final/04.extra-1.js
+++ src/__tests__/final/04.extra-1.js
@@ -6,14 +6,14 @@ import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
 
-test('submitting the form calls onSubmit with username and password', () => {
+test('submitting the form calls onSubmit with username and password', async () => {
   const handleSubmit = jest.fn()
   render(<Login onSubmit={handleSubmit} />)
   const username = 'chucknorris'
   const password = 'i need no password'
 
-  userEvent.type(screen.getByLabelText(/username/i), username)
-  userEvent.type(screen.getByLabelText(/password/i), password)
+  await userEvent.type(screen.getByLabelText(/username/i), username)
+  await userEvent.type(screen.getByLabelText(/password/i), password)
   userEvent.click(screen.getByText(/submit/i))
 
   expect(handleSubmit).toHaveBeenCalledTimes(1)
