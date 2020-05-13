5da904c3fd3465f1e7a5cbc9d64cf0a705db6627
Kent C. Dodds <me+github@kentcdodds.com>
2020-05-13 13:35:25 -0600

    fix test utils

--- src/test/test-utils.js
+++ src/test/test-utils.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {render as rtlRender} from '@testing-library/react'
 import {ThemeProvider} from 'components/theme'
 
-function render(ui, {theme = 'light', ...options}) {
+function render(ui, {theme = 'light', ...options} = {}) {
   const Wrapper = ({children}) => (
     <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
   )
