9b46259e29974831ed12d9f9a98df522a8ed434f
Ricardo Aguilera <ricardo.aguilera@beliveo.com>
2022-05-23 21:40:16 -0500

    adding changes to the test 01 exercise

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -11,25 +11,23 @@ import Counter from '../../components/counter'
 // Luckily, it's handled for you by React Testing Library :)
 global.IS_REACT_ACT_ENVIRONMENT = true
 
-test('counter increments and decrements when the buttons are clicked', () => {
-  // 🐨 create a div to render your component to (💰 document.createElement)
-  //
-  // 🐨 append the div to document.body (💰 document.body.append)
-  //
-  // 🐨 use createRoot to render the <Counter /> to the div
-  // 🐨 get a reference to the increment and decrement buttons:
-  //   💰 div.querySelectorAll('button')
-  // 🐨 get a reference to the message div:
-  //   💰 div.firstChild.querySelector('div')
-  //
-  // 🐨 expect the message.textContent toBe 'Current count: 0'
-  // 🐨 click the increment button (💰 act(() => increment.click()))
-  // 🐨 assert the message.textContent
-  // 🐨 click the decrement button (💰 act(() => decrement.click()))
-  // 🐨 assert the message.textContent
-  //
-  // 🐨 cleanup by removing the div from the page (💰 div.remove())
-  // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
+beforeEach(() => {
+  document.body.innerHTML = ''
 })
 
-/* eslint no-unused-vars:0 */
+test('counter increments and decrements when the buttons are clicked', () => {
+  const div = document.createElement('div')
+  document.body.append(div)
+
+  const root = createRoot(div)
+  act(() => root.render(<Counter />))
+  console.log(root)
+  const [decrement, increment] = div.querySelectorAll('button')
+  const message = div.firstChild.querySelector('div')
+
+  expect(message.textContent).toBe('Current count: 0')
+  act(() => increment.click())
+  expect(message.textContent).toBe('Current count: 1')
+  act(() => decrement.click())
+  expect(message.textContent).toBe('Current count: 0')
+})
