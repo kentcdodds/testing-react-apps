a57bd64b18f3fc04d8221807af805169356a233d
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-02-21 15:45:37 -0500

    refactor test with react testing library

--- src/__tests__/exercise/02.js
+++ src/__tests__/exercise/02.js
@@ -2,57 +2,18 @@
 // http://localhost:3000/counter
 
 import * as React from 'react'
-import {act} from 'react-dom/test-utils'
-import {createRoot} from 'react-dom/client'
-// 🐨 import the `render` and `fireEvent` utilities from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
 import Counter from '../../components/counter'
 
-// NOTE: this is a new requirement in React 18
-// https://react.dev/blog/2022/03/08/react-18-upgrade-guide#configuring-your-testing-environment
-// Luckily, it's handled for you by React Testing Library :)
-// 💣 so you can now delete this!
-global.IS_REACT_ACT_ENVIRONMENT = true
-
-// 💣 remove this. React Testing Library does this automatically!
-beforeEach(() => {
-  document.body.innerHTML = ''
-})
-
 test('counter increments and decrements when the buttons are clicked', () => {
-  // 💣 remove these two lines, React Testing Library will create the div for you
-  const div = document.createElement('div')
-  document.body.append(div)
+  const {getAllByRole, getByText} = render(<Counter />)
 
-  // 🐨 swap createRoot and root.render with React Testing Library's render
-  // Note that React Testing Library's render doesn't need you to pass a `div`
-  // so you only need to pass one argument. render returns an object with a
-  // bunch of utilities on it. For now, let's just grab `container` which is
-  // the div that React Testing Library creates for us.
-  // 💰 const {container} = render(<Counter />)
-  const root = createRoot(div)
-  act(() => root.render(<Counter />))
+  const [decrement, increment] = getAllByRole('button')
+  const message = getByText('Current count: 0')
 
-  // 🐨 instead of `div` here you'll want to use the `container` you get back
-  // from React Testing Library
-  const [decrement, increment] = div.querySelectorAll('button')
-  const message = div.firstChild.querySelector('div')
-
-  expect(message.textContent).toBe('Current count: 0')
-
-  // 🐨 replace the next two statements with `fireEvent.click(button)`
-  // 💰 note that you can remove `act` completely!
-  const incrementClickEvent = new MouseEvent('click', {
-    bubbles: true,
-    cancelable: true,
-    button: 0,
-  })
-  act(() => increment.dispatchEvent(incrementClickEvent))
+  fireEvent.click(increment)
   expect(message.textContent).toBe('Current count: 1')
-  const decrementClickEvent = new MouseEvent('click', {
-    bubbles: true,
-    cancelable: true,
-    button: 0,
-  })
-  act(() => decrement.dispatchEvent(decrementClickEvent))
+
+  fireEvent.click(decrement)
   expect(message.textContent).toBe('Current count: 0')
 })
