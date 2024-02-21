1b92fefc9acda033c046e44e5fca7305ef2fbd43
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-02-21 15:48:02 -0500

    refactor to use jest dom functions

--- src/__tests__/exercise/02.js
+++ src/__tests__/exercise/02.js
@@ -12,8 +12,8 @@ test('counter increments and decrements when the buttons are clicked', () => {
   const message = getByText('Current count: 0')
 
   fireEvent.click(increment)
-  expect(message.textContent).toBe('Current count: 1')
+  expect(message).toHaveTextContent('Current count: 1')
 
   fireEvent.click(decrement)
-  expect(message.textContent).toBe('Current count: 0')
+  expect(message).toHaveTextContent('Current count: 0')
 })
