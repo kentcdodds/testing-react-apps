825d11585257ad6f840463ca606deb429a60fb94
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-02-25 22:52:24 -0500

    avoid implementation details by grabbing buttons by role and name

--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -2,16 +2,14 @@
 // http://localhost:3000/counter
 
 import * as React from 'react'
-// 🐨 add `screen` to the import here:
-import {render, fireEvent} from '@testing-library/react'
+import {render, fireEvent, screen} from '@testing-library/react'
 import Counter from '../../components/counter'
 
 test('counter increments and decrements when the buttons are clicked', () => {
-  const {container} = render(<Counter />)
-  // 🐨 replace these with screen queries
-  // 💰 you can use `getByText` for each of these (`getByRole` can work for the button too)
-  const [decrement, increment] = container.querySelectorAll('button')
-  const message = container.firstChild.querySelector('div')
+  render(<Counter />)
+  const decrement = screen.getByRole('button', {name: 'Decrement'})
+  const increment = screen.getByRole('button', {name: 'Increment'})
+  const message = screen.getByText('Current count: 0')
 
   expect(message).toHaveTextContent('Current count: 0')
   fireEvent.click(increment)
