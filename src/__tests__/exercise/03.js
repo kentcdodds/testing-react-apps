091d38a5b5dedcad3cb0f059c0a2f41a63203e87
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-02-25 22:55:36 -0500

    use userEvent to simulate user clicking a button in browser more closely

--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -3,17 +3,20 @@
 
 import * as React from 'react'
 import {render, fireEvent, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Counter from '../../components/counter'
 
-test('counter increments and decrements when the buttons are clicked', () => {
+test('counter increments and decrements when the buttons are clicked', async () => {
+  const user = userEvent.setup()
   render(<Counter />)
+
   const decrement = screen.getByRole('button', {name: 'Decrement'})
   const increment = screen.getByRole('button', {name: 'Increment'})
   const message = screen.getByText('Current count: 0')
 
   expect(message).toHaveTextContent('Current count: 0')
-  fireEvent.click(increment)
+  await user.click(increment)
   expect(message).toHaveTextContent('Current count: 1')
-  fireEvent.click(decrement)
+  await user.click(decrement)
   expect(message).toHaveTextContent('Current count: 0')
 })
