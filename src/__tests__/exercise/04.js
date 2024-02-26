8a00ab22c5aaf6690a8766ce82303caa2970faee
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-02-25 23:50:26 -0500

    fix same form values being return from builder

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,13 +5,13 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import faker from 'faker'
-import {build} from '@jackfranklin/test-data-bot'
+import {build, fake} from '@jackfranklin/test-data-bot'
 import Login from '../../components/login'
 
 const loginFormBuilder = build({
   fields: {
-    username: faker.internet.userName(),
-    password: faker.internet.password(),
+    username: fake(faker => faker.internet.userName()),
+    password: fake(faker => faker.internet.password()),
   },
 })
 
