51aae380bd3230c268615785725ac9b31f020032
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-03-02 18:10:17 -0500

    use test utility functions

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -2,21 +2,10 @@
 // http://localhost:3000/easy-button
 
 import * as React from 'react'
-import {render as rtlRender, screen} from '@testing-library/react'
+import {render, screen} from '../../test/test-utils'
 import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 import userEvent from '@testing-library/user-event'
-
-function render(ui, {theme = 'light', ...options} = {}) {
-  function Wrapper({children}) {
-    return <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
-  }
-
-  return rtlRender(ui, {
-    wrapper: Wrapper,
-    ...options,
-  })
-}
 
 test('renders with the light styles for the light theme', () => {
   render(<EasyButton>Easy</EasyButton>, {theme: 'light'})
